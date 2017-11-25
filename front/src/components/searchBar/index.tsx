8a777272b5a3f1edce8942acdc26e71ed493f0d6
Lauri Telama <late@phz.fi>
2017-11-26 01:16:21 +0200

    Added integration to search bar

--- front/src/components/searchBar/index.tsx
+++ front/src/components/searchBar/index.tsx
@@ -4,11 +4,17 @@ import * as R from 'ramda'
 import * as RR from 'react-router'
 import * as Types from 'types'
 import * as FlightsReducer from 'reducers/flights'
+import * as FlightsActions from 'actions/flights'
 import './style.scss'
 
-export const mapStateToProps = ({  }: Types.AppState) => ({})
+export const mapStateToProps = ({ flights }: Types.AppState) => ({
+  searchString: flights.searchString
+})
 
-export const mapDispatchToProps = (dispatch: Types.Dispatch) => ({})
+export const mapDispatchToProps = (dispatch: Types.Dispatch) => ({
+  onSearchStringChange: (searchString: string) =>
+    dispatch(FlightsActions.setSearchString(searchString))
+})
 
 const StatePropsWitness = (false as true) && mapStateToProps({} as any)
 type StateProps = typeof StatePropsWitness
@@ -32,7 +38,11 @@ export const SearchBar: React.ComponentClass<Props> = enhance(
         <div className="search-bar-component">
           <div className="search-bar-box">
             <i className="fa fa-plane" aria-hidden="true" />
-            <input placeholder="Search for airport by code" />
+            <input
+              value={this.props.searchString}
+              onChange={e => this.props.onSearchStringChange(e.target.value)}
+              placeholder="Search for airport by code"
+            />
           </div>
         </div>
       )
