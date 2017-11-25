356d3e1e57081e25e8b2af32ad4a12a009c2e0d8
Lauri Telama <late@phz.fi>
2017-11-25 23:23:48 +0200

    Updated flight types

--- front/src/types.ts
+++ front/src/types.ts
@@ -108,10 +108,13 @@ export const iFlight = t.intersection([
     connector_flights_10h: t.array(t.string)
   }),
   t.partial({
-    weather_risk: t.number,
-    twitter_risk: t.number,
+    weather_risk_arrival: t.number,
+    weather_risk_departure: t.number,
+    twitter_risk_arrival: t.number,
+    twitter_risk_departure: t.number,
     schedule_risk: t.number,
-    overall_risk: t.number
+    overall_risk: t.number,
+    twitterTrends: t.array(t.string)
   })
 ])
 
