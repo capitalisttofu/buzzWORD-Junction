081ea6cd55e6c52fcc56335d3b27901620cac0aa
Juuso Mikkonen <hello@juusomikkonen.com>
2017-11-26 05:33:36 +0200

    Add site footer

---
+++ front/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import './style.scss'
+
+export default () => (
+  <footer>
+    <p>
+      <b>CoPilot</b> is a <b>Junction 2017</b> project by Henri Simola, Lauri
+      Telama, Eetu Virkki, Juuso Mikkonen and Mikko Karjalainen.
+    </p>
+  </footer>
+)
