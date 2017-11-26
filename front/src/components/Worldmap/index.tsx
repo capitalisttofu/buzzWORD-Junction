a60b99f0735a67f9e0906c435413d9cc90aa30d4
Juuso Mikkonen <hello@juusomikkonen.com>
2017-11-26 02:41:24 +0200

    Add option to plot airports to map

--- front/src/components/Worldmap/index.tsx
+++ front/src/components/Worldmap/index.tsx
@@ -34,6 +34,45 @@ type State = {
   zoom: number
 }
 
+const airportToCoords = {
+  HEL: [60.317199707031, 24.963300704956],
+  ARN: [59.651901245117, 17.918600082397],
+  BCN: [41.297100067139, 2.0784599781036],
+  BRU: [50.901401519800004, 4.48443984985],
+  BUD: [47.436901092499994, 19.255599975599996],
+  CDG: [49.0127983093, 2.54999995232],
+  CPH: [55.617900848389, 12.656000137329],
+  DUS: [51.28950119018555, 6.766779899597168],
+  EDJ: [47.988800048799995, 10.2395000458],
+  EDI: [55.95000076293945, -3.372499942779541],
+  FCO: [41.8002778, 12.2388889],
+  FRA: [50.0333333, 8.5705556],
+  GOT: [57.662799835205, 12.279800415039],
+  ICN: [37.46910095214844, 126.45099639892578],
+  IVL: [68.607299804688, 27.405300140381],
+  KBV: [8.09912014008, 98.9861984253],
+  KIX: [34.42729949951172, 135.24400329589844],
+  KTT: [67.700996398926, 24.846799850464],
+  LHR: [51.4706, -0.461941],
+  LPA: [27.931900024414062, -15.38659954071045],
+  MAD: [40.471926, -3.56264],
+  MAN: [53.35369873046875, -2.2749500274658203],
+  MUC: [48.353801727295, 11.786100387573],
+  MXP: [45.6306, 8.72811],
+  NGO: [34.8583984375, 136.80499267578125],
+  NRT: [35.7647018433, 140.386001587],
+  OSL: [60.193901062012, 11.100399971008],
+  OUL: [64.930099487305, 25.354600906372],
+  PRG: [50.1008, 14.26],
+  PVG: [31.143400192260742, 121.80500030517578],
+  RVN: [66.564796447754, 25.830400466919],
+  SGN: [10.8187999725, 106.652000427],
+  TFS: [28.044500351, -16.5725002289],
+  VAA: [63.050701141357, 21.762199401855],
+  VIE: [48.110298156738, 16.569700241089],
+  ZRH: [47.464698791504, 8.5491695404053]
+}
+
 const enhance = connect<StateProps, {}, Props>(mapStateToProps)
 
 export const Worldmap: React.ComponentClass<{}> = enhance(
@@ -108,22 +147,20 @@ export const Worldmap: React.ComponentClass<{}> = enhance(
                     ))}
                 </Geographies>
                 <Markers>
-                  {datapoints.map(
-                    ({ coordinates, radius }: Types.DataPoint) => (
-                      <Marker
-                        marker={{ coordinates }}
-                        key={coordinates.toString()}
-                        onClick={this.toggleZoomToDataPoint}
-                      >
-                        <circle
-                          cx={0}
-                          cy={0}
-                          r={radius}
-                          fill="rgba(100,50,0,0.5)"
-                        />
-                      </Marker>
-                    )
-                  )}
+                  {Object.keys(airportToCoords).map((key: string) => (
+                    <Marker
+                      marker={{
+                        coordinates: [
+                          airportToCoords[key][1],
+                          airportToCoords[key][0]
+                        ]
+                      }}
+                      key={key}
+                      onClick={this.toggleZoomToDataPoint}
+                    >
+                      <circle cx={0} cy={0} r={10} fill="rgba(100,50,0,0.5)" />
+                    </Marker>
+                  ))}
                 </Markers>
               </ZoomableGroup>
             </ComposableMap>
